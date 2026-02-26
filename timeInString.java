d373d31ae3268a3ddac85f4f238798f7660ce713
farooghnaush <farooghnaushahi@gmail.com>
2026-02-26 15:47:30 +0100

    Add files via upload

---
+++ timeInString.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.returnbestcombination;
+
+/**
+ *
+ * @author Faroogh
+ */
+public class timeInString {
+    public String solution(int T) {
+        // Implement your solution here
+        int min, sec, hour, remhour;
+        String b=T+"";
+        if(T<60){
+            b=T+"s";   
+        }
+        if(T>60){
+            sec=T%60;
+            min=T/60;
+            b=min+"m"+sec+"s";
+        }
+        
+        if(T>3600){
+            hour=T/3600;
+            remhour=T%3600;
+            min=(remhour/60);
+            sec=T%60;
+            b=hour+"h"+min+"m"+sec+"s";
+        }
+        return b;
+    }
+}
